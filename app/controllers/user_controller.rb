8f4db518a05c625fd5ce22f5c0b6a265e868f7c7
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-07-03 18:15:03 -0500

    update controller -remove @ from line 11

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,7 +11,7 @@ class UserController < ApplicationController
     def create
       @user = User.new params.require(:user).permit(:email, :password, :password_confirmation)
       if @user.save
-        session [:user_id] = @user.id
+        session [:user_id] = user.id
         redirect_to root_path, notice: "Welcome to the clique!"
       else
         render :new
